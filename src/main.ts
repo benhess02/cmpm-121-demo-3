16d5d939453f2228246caa63d2b90f17c6ff4fc6
Ben Hess <pilotben@comcast.net>
2024-11-07 22:04:58 -0800

    Added test button.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-// todo
+let btn = document.createElement("button");
+btn.innerHTML = "Click me!";
+btn.addEventListener("click", (ev) => {
+    alert("you clicked the button!");
+});
+document.body.append(btn);
