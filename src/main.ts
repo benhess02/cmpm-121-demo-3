108a9f6eb041011a3c34284586999ef461b51345
Ben Hess <pilotben@comcast.net>
2024-11-07 22:11:17 -0800

    Fix linter errors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-let btn = document.createElement("button");
+const btn = document.createElement("button");
 btn.innerHTML = "Click me!";
-btn.addEventListener("click", (ev) => {
+btn.addEventListener("click", () => {
   alert("you clicked the button!");
 });
 document.body.append(btn);
