9ab9feab0fc896f868825fbc6e62eed9574374ad
Ben Hess <pilotben@comcast.net>
2024-11-13 01:27:35 -0800

    Finished part B: Changed space and coin representations

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ import "./leafletWorkaround.ts";
 
 import luck from "./luck.ts";
 
+import { Board, Coin } from "./board.ts";
+
 // Starting location
 const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
@@ -32,61 +34,88 @@ leaflet
   })
   .addTo(map);
 
+const board = new Board(TILE_DEGREES, NEIGHBORHOOD_SIZE);
+
 const playerMarker = leaflet.marker(OAKES_CLASSROOM);
 playerMarker.bindTooltip("You are here!");
 playerMarker.addTo(map);
 
 // Display the player's points
-let playerPoints = 0;
+const playerCoins: Coin[] = [];
 const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
-statusPanel.innerHTML = "No points yet...";
+statusPanel.innerHTML = `${playerCoins.length} coins`;
 
 // Add caches to the map by cell numbers
 function spawnCache(x: number, y: number) {
-  const origin = OAKES_CLASSROOM;
-  const bounds = leaflet.latLngBounds([
-    [origin.lat + x * TILE_DEGREES, origin.lng + y * TILE_DEGREES],
-    [origin.lat + (x + 1) * TILE_DEGREES, origin.lng + (y + 1) * TILE_DEGREES],
-  ]);
+  const cell = board.getCellForPoint(leaflet.latLng(x, y))!;
 
-  const rect = leaflet.rectangle(bounds);
+  const rect = leaflet.rectangle(board.getCellBounds(cell));
   rect.addTo(map);
 
-  let pointValue = Math.floor(luck([x, y, "initialValue"].toString()) * 100);
+  if (cell.coins == null) {
+    const pointValue = Math.floor(
+      luck([x, y, "initialValue"].toString()) * 100,
+    );
+    cell.coins = [];
+    for (let i = 0; i < pointValue; i++) {
+      cell.coins.push({ i: cell.i, j: cell.j, serial: i });
+    }
+  }
 
   rect.bindPopup(() => {
     const popupDiv = document.createElement("div");
     popupDiv.innerHTML = `
-                <div>There is a cache here at "${x},${y}". It has value <span id="value">${pointValue}</span>.</div>`;
-    const collectBtn = document.createElement("button");
-    collectBtn.innerHTML = "Collect";
-    collectBtn.addEventListener("click", () => {
-      if (pointValue > 0) {
-        pointValue--;
-        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-          pointValue.toString();
-        playerPoints++;
-        statusPanel.innerHTML = `${playerPoints} points accumulated`;
-      }
-    });
-    popupDiv.append(collectBtn);
+                <div>Cache ${cell.i}:${cell.j}</div>`;
 
-    const despositBtn = document.createElement("button");
-    despositBtn.innerHTML = "Deposit";
-    despositBtn.addEventListener("click", () => {
-      if (playerPoints > 0) {
-        pointValue++;
-        popupDiv.querySelector<HTMLSpanElement>("#value")!.innerHTML =
-          pointValue.toString();
-        playerPoints--;
-        if (playerPoints == 0) {
-          statusPanel.innerHTML = `No points yet...`;
-        } else {
-          statusPanel.innerHTML = `${playerPoints} points accumulated`;
-        }
-      }
+    function addCacheCoin(coin: Coin) {
+      const coinItem = document.createElement("li");
+      coinList.append(coinItem);
+      coinItem.innerHTML = `${coin.i}:${coin.j}#${coin.serial}`;
+      const collectBtn = document.createElement("button");
+      coinItem.append(collectBtn);
+      collectBtn.innerHTML = "Collect";
+      collectBtn.addEventListener("click", () => {
+        console.log(`Collecting ${coin.i}:${coin.j}#${coin.serial}`);
+        cell.coins!.splice(cell.coins!.indexOf(coin), 1);
+        playerCoins.push(coin);
+        coinItem.remove();
+        statusPanel.innerHTML = `${playerCoins.length} coins`;
+        addInventoryCoin(coin);
+      });
+    }
+
+    function addInventoryCoin(coin: Coin) {
+      const coinItem = document.createElement("li");
+      inventoryList.append(coinItem);
+      coinItem.innerHTML = `${coin.i}:${coin.j}#${coin.serial}`;
+      const depositBtn = document.createElement("button");
+      coinItem.append(depositBtn);
+      depositBtn.innerHTML = "Deposit";
+      depositBtn.addEventListener("click", () => {
+        console.log(`Depositing ${coin.i}:${coin.j}#${coin.serial}`);
+        playerCoins.splice(playerCoins.indexOf(coin), 1);
+        cell.coins!.push(coin);
+        coinItem.remove();
+        statusPanel.innerHTML = `${playerCoins.length} coins`;
+        addCacheCoin(coin);
+      });
+    }
+
+    const coinList = document.createElement("ul");
+    coinList.style.overflow = "scroll";
+    coinList.style.maxHeight = "200px";
+    cell.coins!.forEach((coin) => {
+      addCacheCoin(coin);
     });
-    popupDiv.append(despositBtn);
+    popupDiv.append(coinList);
+
+    const inventoryList = document.createElement("ul");
+    inventoryList.style.overflow = "scroll";
+    inventoryList.style.maxHeight = "200px";
+    playerCoins.forEach((coin) => {
+      addInventoryCoin(coin);
+    });
+    popupDiv.append(inventoryList);
 
     return popupDiv;
   });
@@ -95,7 +124,10 @@ function spawnCache(x: number, y: number) {
 for (let x = -NEIGHBORHOOD_SIZE; x < NEIGHBORHOOD_SIZE; x++) {
   for (let y = -NEIGHBORHOOD_SIZE; y < NEIGHBORHOOD_SIZE; y++) {
     if (luck([x, y].toString()) < CACHE_SPAWN_PROBABILITY) {
-      spawnCache(x, y);
+      spawnCache(
+        OAKES_CLASSROOM.lat + (x * TILE_DEGREES),
+        OAKES_CLASSROOM.lng + (y * TILE_DEGREES),
+      );
     }
   }
 }
